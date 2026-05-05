2f56e26b44f3d60cbfb549cfb75d495e77ef7132
Griffin <120704526+GriffinLev039@users.noreply.github.com>
2026-05-05 00:48:43 -0400

    Continuous dark mode

--- lings-cars-website/js/darkmode.js
+++ lings-cars-website/js/darkmode.js
@@ -1,7 +1,13 @@
-// Get the toggle button
 const toggleBtn = document.getElementById("dark-toggle");
 
-// When clicked, switch themes
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
 toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
